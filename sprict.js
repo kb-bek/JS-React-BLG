32582e91198ae36f15b87961259994fa6c213bbe
Kubanychbek Talipov <talipovkuba555@gmail.com>
2024-04-16 22:27:23 +0600

    Added javascript file

---
+++ sprict.js
@@ -0,0 +1,79 @@
+// Alert, Promt, Confirm
+
+/*
+
+console.log('Hello');
+
+alert('Hello admin');
+const answer = prompt('How are you?');
+const answerBoolean = confirm('Are you here?');
+
+
+console.log(answer);
+console.log(answerBoolean);
+
+*/
+
+// Интерполяция
+
+/*
+
+const name = 'Tony';
+const age = 24;
+const job = 'Frontend developer';
+
+console.log('His name is ' + name + ' ');
+console.log(`His name is ${name}. He is ${age}. He is ${job}`);
+
+*/
+
+// Операторы
+
+/*
+const num = 4;
+const str = 'is number';
+
+console.log(num + ' ' + str); // Конкатинация
+console.log(5 + +'10');
+
+let incr = 10;
+let decr = 10;
+
+let a = 5;
+let b = ++a;
+
+console.log(a, b);
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(10 % 3);
+
+const isAdmin = true;
+const isUser = true;
+
+console.log(isAdmin && isUser);
+console.log(isAdmin || isUser); 
+
+console.log(7 == '7');
+console.log(7 === 7);
+console.log(7 != '7');
+console.log(7 !== 7);
+
+*/
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// for (const num in numbers) {
+//     console.log(num);
+// }
+
+
+
+
+
+
+
